358098e4433ef69fa8b8787fcdd4f6d345d4d85a
WasiimSheb <wasimshebalny@gmail.com>
2024-06-28 00:40:27 +0300

    initializing the hpp files

---
+++ node.hpp
@@ -0,0 +1,23 @@
+#ifndef NODE_HPP
+#define NODE_HPP
+
+
+class Node
+{
+private:
+    /* data */
+public:
+    Node(/* args */);
+    ~Node();
+};
+
+Node::Node(/* args */)
+{
+}
+
+Node::~Node()
+{
+}
+
+
+#endif //NODE_HPP
---
+++ tree.hpp
@@ -0,0 +1,23 @@
+#ifndef TREE_HPP
+#define TREE_HPP
+
+class tree
+{
+private:
+    /* data */
+public:
+    tree(/* args */);
+    ~tree();
+    
+};
+
+tree::tree(/* args */)
+{
+}
+
+tree::~tree()
+{
+}
+
+
+#endif // TREE_HPP
